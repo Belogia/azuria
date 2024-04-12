defff89638bd0a12dcdcda5b6c498d45a0bd7c18
gzordrai <40698142+gzordrai@users.noreply.github.com>
2024-04-12 18:34:24 +0200

    Add tsdoc

--- src/core/errors/ApiUrlNotDefinedError.ts
+++ src/core/errors/ApiUrlNotDefinedError.ts
@@ -1,3 +1,6 @@
+/**
+ * Error thrown when the API_URL is not defined in the environment variables.
+ */
 export class ApiUrlNotDefinedError extends Error {
     constructor() {
         super("API_URL is not defined in the environment variables.");
--- src/core/errors/TokenNotDefinedError.ts
+++ src/core/errors/TokenNotDefinedError.ts
@@ -1,3 +1,6 @@
+/**
+ * Error when the TOKEN is not defined in the environment variables.
+ */
 export class TokenNotDefinedError extends Error {
     constructor() {
         super("TOKEN is not defined in the environment variables.");
